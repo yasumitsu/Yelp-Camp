4be99f86fb5fe52ebe301dba670bc9aad8c12405
Kenji <kenjiyasumitsu@gmail.com>
2021-08-15 20:11:34 +0900

    adding error page boilerplate

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -44,6 +45,7 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
 	'/campgrounds',
 	catchAsync(async (req, res, next) => {
+		if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
 		const campground = new Campground(req.body.campground);
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
@@ -84,8 +86,14 @@ app.delete(
 	})
 );
 
+app.all('*', (req, res, next) => {
+	next(new ExpressError('Page not Found', 404))
+});
+
 app.use((err, req, res, next) => {
-	res.send('here we go again...');
+	const {statusCode = 500 } = err;
+	if (!err.message) err.message = 'Something went wrong';
+	res.status(statusCode).render('error', {err})
 });
 
 app.listen(3000, () => {
