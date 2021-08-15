360f50eef4af719272cd1a206b4d49d8ca6fd5c2
Kenji <kenjiyasumitsu@gmail.com>
2021-08-15 20:32:54 +0900

    adding Joi API to handle validation camps

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const { number } = require('joi');
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
@@ -45,7 +48,20 @@ app.get('/campgrounds/new', (req, res) => {
 app.post(
 	'/campgrounds',
 	catchAsync(async (req, res, next) => {
-		if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+		// if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+		const campgroundSchema = Joi.object({
+			campground: Joi.string().required(),
+			price: Joi.number().required().min(0),
+			image: Joi.string().required(),
+			location: Joi.string().required(),
+			description: Joi.string().required()
+		}).required()
+		const {error} = campgroundSchema.validate(req.body);
+
+		if(error){
+			const msg = error.details.map(el => el.message).join(',')
+			throw new ExpressError(msg, 400)
+		}
 		const campground = new Campground(req.body.campground);
 		await campground.save();
 		res.redirect(`/campgrounds/${campground._id}`);
