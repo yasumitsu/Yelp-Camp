d0ab48dfbd1b0381a40f374d120e779f6b3d725b
Kenji <kenjiyasumitsu@gmail.com>
2021-08-17 20:27:40 +0900

    created routes for review

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const { number } = require('joi');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
@@ -100,6 +101,18 @@ app.delete(
 		const { id } = req.params;
 		await Campground.findByIdAndDelete(id);
 		res.redirect('/campgrounds');
+	})
+);
+
+app.post(
+	'/campgrounds/:id/reviews',
+	catchAsync(async (req, res) => {
+		const campground = await Campground.findById(req.params.id);
+		const review = new Review(req.body.review);
+		campground.reviews.push(review);
+		review.save();
+		campground.save();
+		res.redirect(`/campgrounds/${campground._id}`);
 	})
 );
 
--- models/review.js
+++ models/review.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Schema = monogoose.Schema;
+const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
 	body: String,
