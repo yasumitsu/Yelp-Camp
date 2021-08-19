013f35a1dd02a52be253134d5aa0d90ffbef9b4d
Kenji <kenjiyasumitsu@gmail.com>
2021-08-19 19:09:17 +0900

    removed name

--- models/review.js
+++ models/review.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
 	body: String,
-	rating: Number,
-	name: String
+	rating: Number
 });
 
 module.exports = mongoose.model('Review', reviewSchema);
