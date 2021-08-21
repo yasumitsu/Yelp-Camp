c5074e95a8bf33a00a5b916757feea578716cffe
Kenji <kenjiyasumitsu@gmail.com>
2021-08-21 20:22:12 +0900

    add public directory

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const Review = require('./models/review');
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
 	useCreateIndex: true,
-	useUnifiedTopology: true
+	useUnifiedTopology: true,
+	useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -29,6 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const validateCampground = (req, res, next) => {
 	const { error } = campgroundSchema.validate(req.body);
